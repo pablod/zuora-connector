feb1f37a1a01ff7a63bddd075f32655c9b1df960
Emiliano Lesende <emiliano.lesende@gmail.com>
2011-10-03 10:49:38 -0300

    Special ID field treatment

--- src/main/java/org/mule/modules/zuora/zuora/api/ZObjectMapper.java
+++ src/main/java/org/mule/modules/zuora/zuora/api/ZObjectMapper.java
@@ -10,37 +10,40 @@
 
 package org.mule.modules.zuora.zuora.api;
 
-import org.mule.modules.zuora.zobject.ZObjectType;
-
 import com.zuora.api.object.ZObject;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Transformer;
+import org.mule.modules.zuora.zobject.ZObjectType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Transformer;
+public class ZObjectMapper {
+    private static Logger logger = LoggerFactory.getLogger(ZObjectMapper.class);
 
-public class ZObjectMapper
-{
-    public static ZObject toZObject(ZObjectType type, Map<String, Object> map)
-    {
+    public static ZObject toZObject(ZObjectType type, Map<String, Object> map) {
         ZObject zobject = type.newInstance();
-        for (Entry<String, Object> entry : map.entrySet())
-        {
-            zobject.setField(entry.getKey(), entry.getValue());
+        for (Entry<String, Object> entry : map.entrySet()) {
+            if (!entry.getKey().equalsIgnoreCase("id")) {
+                zobject.setField(entry.getKey(), entry.getValue());
+            } else {
+                if (!(entry.getValue() instanceof String)) {
+                    logger.error("The ID of a ZObject must be a String");
+                }
+                zobject.setId((String) entry.getValue());
+            }
         }
         return zobject;
     }
 
     @SuppressWarnings("unchecked")
-    public static List<ZObject> toZObject(final ZObjectType type, List<Map<String, Object>> maps)
-    {
-        return (List<ZObject>) CollectionUtils.collect(maps, new Transformer()
-        {
+    public static List<ZObject> toZObject(final ZObjectType type, List<Map<String, Object>> maps) {
+        return (List<ZObject>) CollectionUtils.collect(maps, new Transformer() {
             @Override
-            public Object transform(Object input)
-            {
+            public Object transform(Object input) {
                 return toZObject(type, (Map<String, Object>) input);
             }
         });
