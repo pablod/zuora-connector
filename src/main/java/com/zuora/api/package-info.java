45ad9159876f992d3ee9dc5f27ef3ca1d4837a60
Pablo Diez <pablo@zauberlabs.com>
2012-08-29 13:06:49 -0300

    Adding licenses

--- src/main/java/com/zuora/api/package-info.java
+++ src/main/java/com/zuora/api/package-info.java
@@ -1,2 +1,12 @@
+/**
+ * Mule Zuora Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ *
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://api.zuora.com/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.zuora.api;
